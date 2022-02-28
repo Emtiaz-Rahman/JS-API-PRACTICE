b9dc1812e9f70e02694495d2ab073cd4e4fbc2f7
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-28 13:13:39 +0600

    final

--- The CocktailDB/js/cocktail.js
+++ The CocktailDB/js/cocktail.js
@@ -19,7 +19,7 @@ const displaySearchResult = drinks => {
         const { strDrink, strGlass } = drink
         div.innerHTML = `
         <div class="col">
-        <div class="card h-100">
+        <div onclick="loadCocktaiDetails(${drink.idDrink})" class="card h-100">
             <img src="${drink.strDrinkThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${strDrink}</h5>
@@ -39,4 +39,33 @@ const displaySearchResult = drinks => {
 
     })
 
+}
+const loadCocktaiDetails = drinkId => {
+    console.log(drinkId);
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`
+    fetch(url)
+        .then(Response => Response.json())
+        .then(data => displayCocktaiDetails(data.drinks[0]))
+}
+const displayCocktaiDetails = drink => {
+    const cocktailDetails = document.getElementById('cocktail-details')
+    cocktailDetails.textContent = '';
+    const div = document.createElement('div')
+    div.classList.add('card')
+    const { strDrinkThumb, strDrink, strGlass, strCategory } = drink
+    div.innerHTML = `
+    <img src="${strDrinkThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${strDrink}</h5>
+                
+                <h5 class="glass">Glass Type:${strGlass}</h5>
+                
+                <h5 class="card-category">Category:${strCategory}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.</p>
+                  
+                </div>
+
+    `
+    cocktailDetails.appendChild(div)
 }
