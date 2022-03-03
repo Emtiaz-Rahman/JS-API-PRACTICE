c76fa8c7198c2dc26bd33094cec7f5d41dcc2c49
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-03-03 12:33:43 +0600

    set timeout

---
+++ TimeOut/timeOut.js
@@ -0,0 +1,13 @@
+function doSomething() {
+    console.log('I coding javaScript')
+}
+console.log('Gomer jonno jihad no1');
+setTimeout(doSomething, 7000)
+console.log('Terpor no2 holo emtiaz ');
+console.log('Sober picone salman');
+setTimeout(function () {
+    console.log('I am using VS code')
+}, 4000)
+setTimeout(() => {
+    console.log('I am exploring articals')
+}, 4000)
